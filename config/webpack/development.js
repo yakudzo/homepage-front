2141468dc73ab1a57c63b2e8d4a58339af39fe3f
Shohboz <vip.shohboz@gmail.com>
2016-12-17 14:55:54 +0300

    Fix webpack config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,16 +4,12 @@ module.exports = () => ({
   // watch: true,
   devtool: 'source-map',
   devServer: {
-    // contentBase: path.resolve(__dirname, '/backend'),
     host: 'localhost',
     port: 8050,
     historyApiFallback: true,
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000
-    },
-    proxy: {
-      "/api/*": "http://dev.cake.dev"
     }
   }
 });
